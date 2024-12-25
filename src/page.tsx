36b0b61c7115abb2133d3264f91e3e924b73c60e
justinv1234 <justinveltri2005@gmaill.com>
2024-12-25 00:31:34 -0500

    replaced BrowserRouter with HashRouter -_-

--- src/page.tsx
+++ src/page.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar.tsx";
 import Home from "./home/home.tsx";
